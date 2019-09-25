93dedd7601244306a6c87a860b2061b2a94af89a
Charles Naut <cpn585@gmail.com>
2019-09-25 15:48:07 -0400

    Send updates about player location through websockets

--- main.go
+++ main.go
@@ -6,7 +6,22 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Player)
+
+type Player struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("index.html")
@@ -20,17 +35,76 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Define a reader which will listen for new messages being sent to our WebSocket endpoint
+func reader(conn *websocket.Conn) {
+	for {
+		// Read in a message
+		var player Player
+		err := conn.ReadJSON(&player)
+		if err != nil {
+			log.Println(err)
+			delete(clients, conn)
+			return
+		}
+
+		log.Printf("%d", player.X)
+		log.Printf("%d", player.Y)
+
+		broadcast <- player
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// Upgrade this connection to a WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+	if err != nil {
+		log.Println(err)
+	}
+
+	clients[ws] = true
+
+	reader(ws)
+
+	//defer ws.Close()
+}
+
 func setupRoutes() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/ws", wsEndpoint)
 
 	http.Handle("/public/", http.StripPrefix(strings.TrimRight("/public/", "/"), http.FileServer(http.Dir("./public"))))
+}
 
+func handleActions() {
+	log.Println("Handling Actions")
+	for {
+		player := <-broadcast
+		log.Println("Looping")
+		for client := range clients {
+			log.Printf("Client")
+			err := client.WriteJSON(player)
+			if err != nil {
+				log.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
 
 func main() {
 	fmt.Println("Space dots")
 
 	setupRoutes()
+
+	go handleActions()
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- public/script.js
+++ public/script.js
@@ -8,54 +8,76 @@ var deltaX = 0;
 var deltaY = 0;
 
 function drawShip() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Ship
-    context.beginPath();
-    context.moveTo(200 + deltaX, 100 + deltaY);
-    context.lineTo(170 + deltaX, 150 + deltaY);
-    context.lineTo(230 + deltaX, 150 + deltaY);
-    context.closePath();
-    
-    context.fill();
+  // Ship
+  context.beginPath();
+  context.moveTo(200 + deltaX, 100 + deltaY);
+  context.lineTo(170 + deltaX, 150 + deltaY);
+  context.lineTo(230 + deltaX, 150 + deltaY);
+  context.closePath();
+
+  socket.send('{ "X": ' + deltaX + ', "Y": ' + deltaY + '}');
+  
+  context.fill();
+}
+ 
+function keysPressed(e) {
+  // Store entry for every key pressed
+  keys[e.keyCode] = true;
+
+  // Left
+  if (keys[37]) {
+    deltaX -= 2;
+  }
+
+  // Right
+  if (keys[39]) {
+    deltaX += 2;
+  }
+
+  // Down
+  if (keys[38]) {
+    deltaY -= 2;
+  }
+
+  // Up
+  if (keys[40]) {
+    deltaY += 2;
+  }
+
+  e.preventDefault();
+
+  drawShip();
+}
+ 
+function keysReleased(e) {
+  // Mark keys that were released
+  keys[e.keyCode] = false;
 }
 
-drawShip();
 window.addEventListener("keydown", keysPressed, false);
 window.addEventListener("keyup", keysReleased, false);
  
 var keys = [];
- 
-function keysPressed(e) {
-    // Store entry for every key pressed
-    keys[e.keyCode] = true;
- 
-    // Left
-    if (keys[37]) {
-      deltaX -= 2;
-    }
- 
-    // Right
-    if (keys[39]) {
-      deltaX += 2;
-    }
- 
-    // Down
-    if (keys[38]) {
-      deltaY -= 2;
-    }
- 
-    // Up
-    if (keys[40]) {
-      deltaY += 2;
-    }
- 
-    e.preventDefault();
- 
-    drawShip();
-}
- 
-function keysReleased(e) {
-    // Mark keys that were released
-    keys[e.keyCode] = false;
-}
+
+var socket = new WebSocket("ws://127.0.0.1:8080/ws");
+console.log("Attempting Connection...");
+
+socket.onopen = () => {
+  console.log("Successfully Connected");
+  drawShip();
+};
+
+socket.onclose = event => {
+  console.log("Socket Closed Connection: ", event);
+  socket.send("Client Closed!")
+};
+
+socket.onerror = error => {
+  console.log("Socket Error: ", error);
+};
+
+socket.onmessage = function (event) {
+  console.log(event.data);
+};
