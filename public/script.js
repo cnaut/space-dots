2639ec3df000fb57be0a1486aa92dcf0f0f643d2
Charles Naut <cpn585@gmail.com>
2019-11-15 12:17:26 -0500

    Use typescript for script file

--- public/script.js
+++ public/script.js
@@ -1,105 +1,83 @@
-
 var canvas = document.querySelector("#playground");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var context = canvas.getContext("2d");
- 
 var deltaX = 0;
 var deltaY = 0;
-
 var keys = [];
 var thisPlayer = {
-  x: Math.floor(Math.random() * 1000),
-  y: Math.floor(Math.random() * 1000)
-}
-
+    x: Math.floor(Math.random() * 1000),
+    y: Math.floor(Math.random() * 1000),
+    id: undefined
+};
 var socket = new WebSocket("ws://127.0.0.1:8080/ws");
-
 function clearPlayer(player) {
-  context.clearRect(player.x - 2, player.y -2, 64, 54);
+    context.clearRect(player.x - 2, player.y - 2, 64, 54);
 }
-
 function drawPlayer(player) {
-  console.log(player);
-  // Ship
-  context.beginPath();
-  context.moveTo(30 + player.x, player.y);
-  context.lineTo(player.x, 50 + player.y);
-  context.lineTo(60 + player.x, 50 + player.y);
-  context.closePath();
-  
-  context.fill();
+    console.log(player);
+    // Ship
+    context.beginPath();
+    context.moveTo(30 + player.x, player.y);
+    context.lineTo(player.x, 50 + player.y);
+    context.lineTo(60 + player.x, 50 + player.y);
+    context.closePath();
+    context.fill();
 }
- 
-function keysPressed(e, sendUpdate) {
-  clearPlayer(thisPlayer);
-
-  // Store entry for every key pressed
-  keys[e.keyCode] = true;
-
-  // Left
-  if (keys[37]) {
-    thisPlayer.x -= 2;
-  }
-
-  // Right
-  if (keys[39]) {
-    thisPlayer.x += 2;
-  }
-
-  // Down
-  if (keys[38]) {
-    thisPlayer.y -= 2;
-  }
-
-  // Up
-  if (keys[40]) {
-    thisPlayer.y += 2;
-  }
-
-  e.preventDefault();
-
-  drawPlayer(thisPlayer);
-
-  socket.send('{ "id": ' + thisPlayer.id + ', "x": ' + thisPlayer.x + ', "y": ' + thisPlayer.y + ' }');
+var increment = 8;
+function keysPressed(e) {
+    clearPlayer(thisPlayer);
+    // Store entry for every key pressed
+    keys[e.keyCode] = true;
+    // Left
+    if (keys[37]) {
+        thisPlayer.x -= increment;
+    }
+    // Right
+    if (keys[39]) {
+        thisPlayer.x += increment;
+    }
+    // Down
+    if (keys[38]) {
+        thisPlayer.y -= increment;
+    }
+    // Up
+    if (keys[40]) {
+        thisPlayer.y += increment;
+    }
+    e.preventDefault();
+    drawPlayer(thisPlayer);
+    socket.send('{ "id": ' + thisPlayer.id + ', "x": ' + thisPlayer.x + ', "y": ' + thisPlayer.y + ' }');
 }
- 
 function keysReleased(e) {
-  // Mark keys that were released
-  keys[e.keyCode] = false;
+    // Mark keys that were released
+    keys[e.keyCode] = false;
 }
-
 window.addEventListener("keydown", keysPressed, false);
 window.addEventListener("keyup", keysReleased, false);
- 
 console.log("Attempting Connection...");
-
-socket.onopen = () => {
-  console.log("Successfully Connected");
-  drawPlayer(thisPlayer);
+socket.onopen = function () {
+    console.log("Successfully Connected");
+    drawPlayer(thisPlayer);
 };
-
-socket.onclose = event => {
-  console.log("Socket Closed Connection: ", event);
-  socket.send("Client Closed!")
+socket.onclose = function (event) {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!");
 };
-
-socket.onerror = error => {
-  console.log("Socket Error: ", error);
+socket.onerror = function (error) {
+    console.log("Socket Error: ", error);
 };
-
 socket.onmessage = function (event) {
-  if (!event.data) {
-    return;
-  }
-
-  console.log(event.data);
-
-  var player = JSON.parse(event.data);
-  if (player.self) {
-    thisPlayer.id = player.id;
-  } else {
-    clearPlayer(player)
-    drawPlayer(player)
-  }
+    if (!event.data) {
+        return;
+    }
+    console.log(event.data);
+    var player = JSON.parse(event.data);
+    if (player.self) {
+        thisPlayer.id = player.id;
+    }
+    else {
+        clearPlayer(player);
+        drawPlayer(player);
+    }
 };
---
+++ public/script.ts
@@ -0,0 +1,107 @@
+
+const canvas : any = <HTMLCanvasElement> document.querySelector("#playground");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const context = canvas.getContext("2d");
+ 
+let deltaX = 0;
+let deltaY = 0;
+
+let keys = [];
+let thisPlayer = {
+  x: Math.floor(Math.random() * 1000),
+  y: Math.floor(Math.random() * 1000),
+  id: undefined
+}
+
+const socket = new WebSocket("ws://127.0.0.1:8080/ws");
+
+function clearPlayer(player) {
+  context.clearRect(player.x - 2, player.y -2, 64, 54);
+}
+
+function drawPlayer(player) {
+  console.log(player);
+  // Ship
+  context.beginPath();
+  context.moveTo(30 + player.x, player.y);
+  context.lineTo(player.x, 50 + player.y);
+  context.lineTo(60 + player.x, 50 + player.y);
+  context.closePath();
+  
+  context.fill();
+}
+ 
+const increment = 8;
+function keysPressed(e) {
+  clearPlayer(thisPlayer);
+
+  // Store entry for every key pressed
+  keys[e.keyCode] = true;
+
+  // Left
+  if (keys[37]) {
+    thisPlayer.x -= increment;
+  }
+
+  // Right
+  if (keys[39]) {
+    thisPlayer.x += increment;
+  }
+
+  // Down
+  if (keys[38]) {
+    thisPlayer.y -= increment;
+  }
+
+  // Up
+  if (keys[40]) {
+    thisPlayer.y += increment;
+  }
+
+  e.preventDefault();
+
+  drawPlayer(thisPlayer);
+
+  socket.send('{ "id": ' + thisPlayer.id + ', "x": ' + thisPlayer.x + ', "y": ' + thisPlayer.y + ' }');
+}
+ 
+function keysReleased(e) {
+  // Mark keys that were released
+  keys[e.keyCode] = false;
+}
+
+window.addEventListener("keydown", keysPressed, false);
+window.addEventListener("keyup", keysReleased, false);
+ 
+console.log("Attempting Connection...");
+
+socket.onopen = () => {
+  console.log("Successfully Connected");
+  drawPlayer(thisPlayer);
+};
+
+socket.onclose = event => {
+  console.log("Socket Closed Connection: ", event);
+  socket.send("Client Closed!")
+};
+
+socket.onerror = error => {
+  console.log("Socket Error: ", error);
+};
+
+socket.onmessage = function (event) {
+  if (!event.data) {
+    return;
+  }
+
+  console.log(event.data);
+
+  let player = JSON.parse(event.data);
+  if (player.self) {
+    thisPlayer.id = player.id;
+  } else {
+    clearPlayer(player)
+    drawPlayer(player)
+  }
+};
